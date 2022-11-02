7c808ebd815e6316fd7c204440551a24844916a3
Alfredo Deza <alfredo@deza.pe>
2022-11-02 10:50:53 -0400

    Update create.sql

--- sql_files/create.sql
+++ sql_files/create.sql
@@ -2,10 +2,10 @@
 --CREATE DATABASE medalists;
 --  ENCODING 'UTF8';
 --
---CREATE TABLE IF NOT EXISTS medalists (
--- 	id integer primary key,
--- 	place VARCHAR(60),
--- 	athlete TEXT,
---    height TEXT,
---    year TEXT,
---    );
+CREATE TABLE IF NOT EXISTS medalists (
+ 	id integer primary key,
+ 	place VARCHAR(60),
+ 	athlete TEXT,
+    height TEXT,
+    year TEXT,
+    );
